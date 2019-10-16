ea71791dcfa284ae0b4d1ff8847dc5c58a8b6f08
Alex Root Junior <jroot.junior@gmail.com>
2019-10-16 23:20:57 +0300

    Add chat type column

--- src/app/middlewares/acl.py
+++ src/app/middlewares/acl.py
@@ -11,13 +11,14 @@ class ACLMiddleware(BaseMiddleware):
     async def setup_chat(self, data: dict, user: types.User, chat: Optional[types.Chat] = None):
         user_id = user.id
         chat_id = chat.id if chat else user.id
+        chat_type = chat.type if chat else "private"
 
         user = await User.get(user_id)
         if user is None:
             user = await User.create(id=user_id)
         chat = await Chat.get(chat_id)
         if chat is None:
-            chat = await Chat.create(id=chat_id)
+            chat = await Chat.create(id=chat_id, type=chat_type)
 
         data["user"] = user
         data["chat"] = chat
--- src/app/models/chat.py
+++ src/app/models/chat.py
@@ -7,5 +7,7 @@ from app.models.db import BaseModel
 
 class Chat(BaseModel):
     __tablename__ = "chats"
+
     id = sa.Column(sa.BigInteger, primary_key=True, index=True)
+    type = sa.Column(sa.String)
     language = sa.Column(sa.String(12), default="en")
