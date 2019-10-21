bc616452c6b9658319492bcd34b98c859c4ad7e4
Alex Root Junior <jroot.junior@gmail.com>
2019-10-21 22:11:05 +0300

    Auto-run migrations when RUN_MIGRATIONS is placed in environment

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,15 +7,16 @@ APP="${PYTHON} -m app"
 
 # ${PYTHON} before_start.py
 
+if [[ ! -z "${RUN_MIGRATIONS}" ]]; then
+  alembic upgrade head
+fi
+
 case "$1" in
 run-webhook)
   ${APP} webhook
   ;;
 run-polling)
   ${APP} polling
-  ;;
-migrate)
-  alembic upgrade head
   ;;
 *)
   ${@}
