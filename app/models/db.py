6a9e3e57a37ec48d1532932dd13927bf8ca70a65
jrootjunior <jroot.junior@gmail.com>
2019-10-29 13:34:38 +0200

    Add DB Models string representation (based on model name and primary keys)

--- app/models/db.py
+++ app/models/db.py
@@ -1,5 +1,7 @@
 import datetime
+from typing import List
 
+import sqlalchemy as sa
 from aiogram import Dispatcher
 from aiogram.utils.executor import Executor
 from gino import Gino
@@ -12,6 +14,17 @@ db = Gino()
 
 class BaseModel(db.Model):
     __abstract__ = True
+
+    def __str__(self):
+        model = self.__class__.__name__
+        table: sa.Table = sa.inspect(self.__class__)
+        primary_key_columns: List[sa.Column] = table.primary_key.columns
+        values = {
+            column.name: getattr(self, self._column_name_map[column.name])
+            for column in primary_key_columns
+        }
+        values_str = " ".join(f"{name}={value!r}" for name, value in values.items())
+        return f"<{model} {values_str}>"
 
 
 class TimedBaseModel(BaseModel):
