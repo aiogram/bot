27343e5a1fe5243c3a6b7512d4ce1beff9e63b89
jrootjunior <jroot.junior@gmail.com>
2019-10-29 13:54:39 +0200

    Reorder handlers

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -4,8 +4,8 @@ Import all submodules here
 isort:skip_file
 """
 
+from . import simple_admin
 from . import base
 from . import chat_settings
-from . import simple_admin
 from . import new_chat_members
 from . import superuser
